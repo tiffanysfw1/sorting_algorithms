0f252ff19b66644deb5b71039e32a41dcf9bd4e7
coder <122115043+tiffanysfw1@users.noreply.github.com>
2023-04-25 14:50:38 +0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,43 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ *@prev: Pointer to the previous element of the list
+ *@next: Pointer to the next lement of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/*Provided Function prototypes */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+/* My functions prototypes*/
+void swap(int *x, int *y);
+size_t lomuto_partition(int *array, int low, int high, size_t size);
+void lomuto_scheme(int *array, int low, int high, size_t size);
+
+/* Advanced/optional tasks function prototypes*/
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+#endif /* SORT_H */
